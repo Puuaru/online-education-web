24f1a3bcaf2bc56b169ca05a05a8636d8f286a67
Puuaru <honghuizhang4w@gmail.com>
2023-01-13 20:48:00 +0800

    feature: header & footer

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,10 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'online-education-web',
-    htmlAttrs: {
-      lang: 'en',
-    },
+    title: '在线教育前端',
+    // htmlAttrs: {
+    //   lang: 'en',
+    // },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -15,10 +15,16 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['element-ui/lib/theme-chalk/index.css'],
+  css: [
+    'element-ui/lib/theme-chalk/index.css',
+    'swiper/swiper-bundle.css'
+    ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/element-ui'],
+  plugins: [
+    '@/plugins/element-ui',
+    {src: '@/plugins/nuxt-swiper.js', ssr: false}
+    ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
