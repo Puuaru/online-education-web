6b08c3017ed3f8f8c300724daf7dc06ab10342b8
Puuaru <honghuizhang4w@gmail.com>
2023-02-25 20:58:48 +0800

    feat: add course details

--- pages/course/_id.vue
+++ pages/course/_id.vue
@@ -7,34 +7,45 @@
         \
         <a href="#" title class="c-999 fsize14">课程列表</a>
         \
-        <span class="c-333 fsize14">Java精品课程</span>
+        <span class="c-333 fsize14">{{ courseInfo.title }}</span>
       </section>
       <div>
-        <article class="c-v-pic-wrap" style="height: 357px;">
+        <article class="c-v-pic-wrap" style="height: 357px">
           <section class="p-h-video-box" id="videoPlay">
-            <img src="@/assets/photo/course/1442295581911.jpg" alt="Java精品课程" class="dis c-v-pic">
+            <img
+              style="object-fit: cover; width: 100%; height: 100%"
+              :src="courseInfo.cover"
+              :alt="courseInfo.title"
+              class="dis c-v-pic"
+            />
           </section>
         </article>
         <aside class="c-attr-wrap">
           <section class="ml20 mr15">
             <h2 class="hLh30 txtOf mt15">
-              <span class="c-fff fsize24">Java精品课程</span>
+              <span class="c-fff fsize24">{{ courseInfo.title }}</span>
             </h2>
             <section class="c-attr-jg">
               <span class="c-fff">价格：</span>
-              <b class="c-yellow" style="font-size:24px;">￥0.00</b>
+              <b class="c-yellow" style="font-size: 24px"
+                >￥{{ courseInfo.price }}</b
+              >
             </section>
             <section class="c-attr-mt c-attr-undis">
-              <span class="c-fff fsize14">主讲： 唐嫣&nbsp;&nbsp;&nbsp;</span>
+              <span class="c-fff fsize14"
+                >主讲： {{ courseInfo.teacherName }}&nbsp;&nbsp;&nbsp;</span
+              >
             </section>
             <section class="c-attr-mt of">
               <span class="ml10 vam">
-                <em class="icon18 scIcon"></em>
-                <a class="c-fff vam" title="收藏" href="#" >收藏</a>
+                <a class="c-fff vam" href="/login" v-if="!isLogined"
+                  >如未登录，请先登录</a
+                >
               </span>
             </section>
             <section class="c-attr-mt">
               <a href="#" title="立即观看" class="comm-btn c-btn-3">立即观看</a>
+              <!-- <a href="#" title="立即观看" class="comm-btn c-btn-3">立即观看</a> -->
             </section>
           </section>
         </aside>
@@ -44,24 +55,24 @@
               <p>&nbsp;</p>
               <aside>
                 <span class="c-fff f-fM">购买数</span>
-                <br>
-                <h6 class="c-fff f-fM mt10">150</h6>
+                <br />
+                <h6 class="c-fff f-fM mt10">{{ courseInfo.buyCount }}</h6>
               </aside>
             </li>
             <li>
               <p>&nbsp;</p>
               <aside>
                 <span class="c-fff f-fM">课时数</span>
-                <br>
-                <h6 class="c-fff f-fM mt10">20</h6>
+                <br />
+                <h6 class="c-fff f-fM mt10">{{ courseInfo.lessonNum }}</h6>
               </aside>
             </li>
             <li>
               <p>&nbsp;</p>
               <aside>
                 <span class="c-fff f-fM">浏览数</span>
-                <br>
-                <h6 class="c-fff f-fM mt10">501</h6>
+                <br />
+                <h6 class="c-fff f-fM mt10">{{ courseInfo.viewCount }}</h6>
               </aside>
             </li>
           </ol>
@@ -85,16 +96,10 @@
                   </h6>
                   <div class="course-txt-body-wrap">
                     <section class="course-txt-body">
-                      <p>
-                        Java的发展历史，可追溯到1990年。当时Sun&nbsp;Microsystem公司为了发展消费性电子产品而进行了一个名为Green的项目计划。该计划
-                        负责人是James&nbsp;Gosling。起初他以C++来写一种内嵌式软件，可以放在烤面包机或PAD等小型电子消费设备里，使得机器更聪明，具有人工智
-                        能。但他发现C++并不适合完成这类任务！因为C++常会有使系统失效的程序错误，尤其是内存管理，需要程序设计师记录并管理内存资源。这给设计师们造成
-                        极大的负担，并可能产生许多bugs。&nbsp;
-                        <br>为了解决所遇到的问题，Gosling决定要发展一种新的语言，来解决C++的潜在性危险问题，这个语言名叫Oak。Oak是一种可移植性语言，也就是一种平台独立语言，能够在各种芯片上运行。
-                        <br>1994年，Oak技术日趋成熟，这时网络正开始蓬勃发展。Oak研发小组发现Oak很适合作为一种网络程序语言。因此发展了一个能与Oak配合的浏
-                        览器--WebRunner，后更名为HotJava，它证明了Oak是一种能在网络上发展的程序语言。由于Oak商标已被注册，工程师们便想到以自己常
-                        享用的咖啡(Java)来重新命名，并于Sun&nbsp;World&nbsp;95中被发表出来。
-                      </p>
+                      <!-- <p> -->
+                      <!--   {{ courseInfo.description }} -->
+                      <!-- </p> -->
+                      <div v-html="courseInfo.description"></div>
                     </section>
                   </div>
                 </div>
@@ -109,21 +114,27 @@
                         <ul>
                           <!-- 文件目录 -->
                           <li class="lh-menu-stair">
-                            <a href="javascript: void(0)" title="第一章" class="current-1">
+                            <a
+                              href="javascript: void(0)"
+                              title="第一章"
+                              class="current-1"
+                            >
                               <em class="lh-menu-i-1 icon18 mr10"></em>第一章
                             </a>
-                            <ol class="lh-menu-ol" style="display: block;">
+                            <ol class="lh-menu-ol" style="display: block">
                               <li class="lh-menu-second ml30">
                                 <a href="#" title>
                                   <span class="fr">
                                     <i class="free-icon vam mr10">免费试听</i>
                                   </span>
-                                  <em class="lh-menu-i-2 icon16 mr5">&nbsp;</em>第一节
+                                  <em class="lh-menu-i-2 icon16 mr5">&nbsp;</em
+                                  >第一节
                                 </a>
                               </li>
                               <li class="lh-menu-second ml30">
                                 <a href="#" title class="current-2">
-                                  <em class="lh-menu-i-2 icon16 mr5">&nbsp;</em>第二节
+                                  <em class="lh-menu-i-2 icon16 mr5">&nbsp;</em
+                                  >第二节
                                 </a>
                               </li>
                             </ol>
@@ -145,11 +156,16 @@
                 <a title href="javascript:void(0)">主讲讲师</a>
               </section>
               <section class="stud-act-list">
-                <ul style="height: auto;">
+                <ul style="height: auto">
                   <li>
                     <div class="u-face">
                       <a href="#">
-                        <img src="@/assets/photo/teacher/1442297969808.jpg" width="50" height="50" alt>
+                        <img
+                          src="@/assets/photo/teacher/1442297969808.jpg"
+                          width="50"
+                          height="50"
+                          alt
+                        />
                       </a>
                     </div>
                     <section class="hLh30 txtOf">
@@ -172,14 +188,16 @@
 </template>
 
 <script>
-import course from '@/api/course';
+import course from '@/api/course'
+import cookie from 'js-cookie'
 
 export default {
   data() {
     return {
-      courseId : '',
+      courseId: '',
       courseInfo: {},
-      chapters: {}
+      chapters: {},
+      isLogined: null,
     }
   },
 
@@ -188,6 +206,7 @@ export default {
       this.courseId = this.$route.params.id
     }
     this.getCourseDetails()
+    this.getLoginStatus()
   },
 
   methods: {
@@ -198,7 +217,16 @@ export default {
           this.chapters = respond.data.data.chapters
         })
       }
-    }
-  }
-};
+    },
+
+    getLoginStatus() {
+      let cookieValue = cookie.get('EDUSER')
+      if (cookieValue) {
+        this.isLogined = true
+        return
+      }
+      this.isLogined = false
+    },
+  },
+}
 </script>
