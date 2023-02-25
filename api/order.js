2ebf5593b76a42dee8f5338e3320cde5484b3758
Puuaru <honghuizhang4w@gmail.com>
2023-02-25 20:58:33 +0800

    feat: add order api

---
+++ api/order.js
@@ -0,0 +1,24 @@
+import request from '@/utils/request'
+
+export default {
+  saveOrder(courseId) {
+    return request({
+      url: `/order/operation/${courseId}`,
+      method: 'post',
+    })
+  },
+
+  getOrder(orderNo) {
+    return request({
+      url: `/order/operation/${orderNo}`,
+      method: 'get',
+    })
+  },
+
+  savePayLog(orderNo) {
+    return request({
+      url: `/order/pay-log/${orderNo}`,
+      method: 'post',
+    })
+  },
+}
